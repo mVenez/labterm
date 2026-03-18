0fbba965bf64fd93eca68a2120e576e8a55fe134
mVenez <matteo.veneziano@ikmail.com>
2026-03-18 21:35:48 +0100

    feat: async and indipendent instrument data update
    Previously the data updates started simultaneously for each instrument, but they all needed to complete before the second update was allowed to start.
    Now, the instruments are indipendent from each other and can follow different periods of data updates (if told so in the update_data() method, the dashboard still considers a single update interval for all).

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import sys, os
+from importlib.metadata import version as get_version
 
 sys.path.insert(0, os.path.abspath('../../src'))
 
@@ -12,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'labterm'
 copyright = '2026, Matteo Veneziano'
 author = 'Matteo Veneziano'
-release = '1.0.0'
+release = get_version("labterm")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
